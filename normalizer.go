252948f46d29ad5d65e6c6bae4fdb7fe27413b35
Piotr <piotr@positionly.com>
2016-06-16 15:18:57 +0200

    fix variables names

--- normalizer.go
+++ normalizer.go
@@ -1,12 +1,12 @@
 package interpolation
 
 func NormalizeMap(m map[int]int, max int, min int) map[int]int {
-	for i, pair := range m {
-		if rate > max {
-			rates[i] = max
-		} else if rate < min {
-			rates[i] = min
+	for i, val := range m {
+		if val > max {
+			m[i] = max
+		} else if val < min {
+			m[i] = min
 		}
 	}
-	return rates
+	return m
 }
