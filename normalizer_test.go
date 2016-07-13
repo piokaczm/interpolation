0478ba3a3a2d3c2319ed9547e9908d7589b1c9c5
Piotr <piotr@positionly.com>
2016-07-13 14:30:14 +0200

    add test for normalizing with min==max

--- normalizer_test.go
+++ normalizer_test.go
@@ -29,3 +29,11 @@ func TestNormalizeArray(t *testing.T) {
 	Expect(t, val1, norm_arr[0])
 	ExpectNotEqual(t, val2, norm_arr[400])
 }
+
+func TestNormalizeWithEqualMinMax(t *testing.T) {
+	norm_arr := m.InterpolationArray(1440)
+	NormalizeArray(norm_arr, 15, 15)
+	Expect(t, 15, norm_arr[0])
+	Expect(t, 15, norm_arr[100])
+	Expect(t, 15, norm_arr[1000])
+}
